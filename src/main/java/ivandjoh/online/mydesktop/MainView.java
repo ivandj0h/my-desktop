c2de16ac64f62163b4dcb2fe87b6884b30e71bfa
ivandjoh <ivandi.djoh@gmail.com>
2022-08-11 06:33:32 +0800

    bug fixed h2 db

--- src/main/java/ivandjoh/online/mydesktop/MainView.java
+++ src/main/java/ivandjoh/online/mydesktop/MainView.java
@@ -1,26 +1,67 @@
 package ivandjoh.online.mydesktop;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.router.Route;
-import ivandjoh.online.mydesktop.entity.User;
+import ivandjoh.online.mydesktop.entity.UserEntity;
 import ivandjoh.online.mydesktop.repository.UserRepository;
-import ivandjoh.online.mydesktop.utils.FormUtils;
 
 @Route("")
 public class MainView extends VerticalLayout {
 
+    private UserRepository userRepository;
+
     private TextField firstName = new TextField("First Name");
     private TextField lastName = new TextField("Last Name");
-    private TextField email = new TextField("Email");
-    private Grid<User> grid = new Grid<>(User.class);
-    FormUtils formUtils = new FormUtils();
+    private EmailField email = new EmailField("Email");
+    private Grid<UserEntity> grid = new Grid<>(UserEntity.class);
+    private Binder<UserEntity> binder = new Binder<>(UserEntity.class);
 
     public MainView(UserRepository userRepository) {
+        this.userRepository = userRepository;
 
         grid.setColumns("firstName", "lastName", "email");
-        add(formUtils.getForm(firstName, lastName, email), grid);
+        add(getForm(), grid);
+
+        refreshGrid();
+    }
+
+    private Component getForm() {
+        var layout = new HorizontalLayout();
+        layout.setAlignItems(FlexComponent.Alignment.BASELINE);
+
+        var addButton = new Button("Add User");
+        addButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        layout.add(firstName, lastName, email, addButton);
+
+        binder.bindInstanceFields(this);
+
+        addButton.addClickListener(e -> {
+            try {
+                var user = new UserEntity();
+                binder.writeBean(user);
+
+                userRepository.save(user);
+            } catch (ValidationException ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        return layout;
+    }
+
+    private void refreshGrid() {
+        grid.setItems(userRepository.findAll());
     }
 
 }
--- src/main/java/ivandjoh/online/mydesktop/repository/UserRepository.java
+++ src/main/java/ivandjoh/online/mydesktop/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package ivandjoh.online.mydesktop.repository;
 
-import ivandjoh.online.mydesktop.entity.User;
+import ivandjoh.online.mydesktop.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> { }
+public interface UserRepository extends JpaRepository<UserEntity, Long> { }
